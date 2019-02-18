c2135f1f7573bfb188b3c8b76348cc4958d5ffe2
tiankun <tkk@job-mate.com>
2019-02-18 17:01:29 +0800

    config fix

---
+++ service/src/main/java/com/jm/coding/service/login/JwtUtil.java
@@ -0,0 +1,10 @@
+package com.jm.coding.service.login;
+
+import java.util.UUID;
+
+public class JwtUtil {
+
+    private static final long EXPIRE_TIME = 15 * 60 * 1000;
+
+    private static String TOKEN_SECRET = "";
+}
